c20a5537b146918c9a8d4bc0d9149021ea6b64f0
Jose Amesquita <joseamesquita11@gmail.com>
2020-02-14 11:30:10 -0800

    rename method in the Doctor class

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,5 +1,5 @@
 export class Doctor {
-  async Doctor() {
+  async findDoctor() {
     try {
       let response = await fetch('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=e47ae5310bdb049364b12288b1e20a98=${process.env.API_KEY}');
       let jsonifiedResponse;
