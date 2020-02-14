1a49c7d4c0e492086dcf860d941809de5935c643
Jose Amesquita <joseamesquita11@gmail.com>
2020-02-14 11:26:56 -0800

    1. will call the API given the user key

---
+++ src/doctor-lookup.js
@@ -0,0 +1,17 @@
+export class Doctor {
+  async Doctor() {
+    try {
+      let response = await fetch('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=e47ae5310bdb049364b12288b1e20a98=${process.env.API_KEY}');
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      }
+      else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      return false;
+    };
+  }
+}
